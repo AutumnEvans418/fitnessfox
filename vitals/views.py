acda0cded2302384b7964a8c1db006a7eadc0dd7
AutumnEvans <AutumnFox418@gmail.com>
2025-04-24 18:34:13 -0500

    charts done

--- vitals/views.py
+++ vitals/views.py
@@ -11,6 +11,28 @@ from rest_framework.response import Response
 from json import dumps 
 from django.core import serializers
 
+from collections import defaultdict
+from typing import Callable, Iterable, TypeVar, Dict, List
+
+import jsonpickle
+
+T = TypeVar('T')
+K = TypeVar('K')
+
+def group_by(items: Iterable[T], key_func: Callable[[T], K]) -> Dict[K, List[T]]:
+    grouped = defaultdict(list)
+    for item in items:
+        grouped[key_func(item)].append(item)
+    return dict(grouped)
+
+class VitalChartData:
+    def __init__(self, date: str, type: str, type_id: int, value: int):
+        self.date = date
+        self.type = type
+        self.type_id = type_id
+        self.value = value
+        pass
+
 # Create your views here.
 class VitalIndexView(ListView, LoginRequiredMixin):
     paginate_by = 5
@@ -27,16 +49,107 @@ class VitalIndexView(ListView, LoginRequiredMixin):
 
         ctx = super().get_context_data(**kwargs)
 
-        #list = ctx['object_list']
         user = self.request.user
 
-        list = Vital.objects.filter(user=user).order_by("-date").all()
+        data = [ VitalChartData(
+            str(r.date)[0:10],
+            r.get_type_display(),
+            r.type,
+            r.value) for r in Vital.objects.filter(user=user).order_by("-date").all()] 
+
+        def getGeneralData(data, type, title):
+            data = sorted([x for x in data if x.type_id == type], key=lambda p: p.date)
+
+            date_data = list(group_by(data, lambda x: x.date).values())
+            labels = [x[0].date for x in date_data]
+
+            result = []
+            for label in labels:
+                values = [x.value for x in data if x.date == label]
+                if values:
+                    value = sum(values)/len(values)
+                    result.append(value)
+                else:
+                    result.append(None)
+            
+            return {
+                "text": title,
+                "type": "line",
+                "min": 0,
+                "max": 150,
+                "labels": labels,
+                "data": [
+                    {
+                        "label": title,
+                        "data": result,
+                        "color": "#23b5d3",                      
+                    }
+                ]
+            }
 
 
 
-        data = serializers.serialize('json', list)
+        def getHeartData(data):
+            data = sorted([x for x in data if x.type_id in [Vital.BPM, Vital.DIASTOLIC, Vital.SYSTOLIC]], key=lambda p: p.date)
 
-        ctx['object_list_json'] = data
+            date_data = list(group_by(data, lambda x: x.date).values())
+
+            labels = [x[0].date for x in date_data]
+
+            bpm = []
+            diastolic = []
+            systolic = []
+
+            for label in labels:
+
+                def addList(type, d):
+                    values = [x.value for x in data if x.date == label and x.type_id == type]
+                    if values:
+                        value = sum(values)/len(values)
+                        d.append(value)
+                    else:
+                        d.append(None)
+                
+                addList(Vital.BPM, bpm)
+                addList(Vital.DIASTOLIC, diastolic)
+                addList(Vital.SYSTOLIC, systolic)
+
+            return {
+                "text": "Heart Vitals",
+                "type": "line",
+                "min": 0,
+                "max": 150,
+                "labels": labels,
+                "data": [
+                    {
+                        "label": "Bpm",
+                        "data": bpm,
+                        "color": "#23b5d3",                      
+                    },
+                    {
+                        "label": "Systolic",
+                        "data": systolic,
+                        "color": "#279af1",                    
+                    },
+                    {
+                        "label": "Diastolic",
+                        "data": diastolic,
+                        "color": "#ea526f",                       
+                    }
+                ]
+            }
+
+        charts = [
+            getHeartData(data),
+            getGeneralData(data, Vital.WEIGHT, "Weight"),
+            getGeneralData(data, Vital.TEMPERATURE, "Temperature")
+        ]
+
+        ctx['object_list_json'] = jsonpickle.encode(data)
+
+        ctx['charts'] = [{
+            "name": chart['text'], 
+            'data': jsonpickle.encode(chart)} for chart in charts] 
 
         mapper = dumps(Vital.VITAL_CHOICES)
 
